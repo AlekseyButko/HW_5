e0e251e5b70e4861fbafeea3d07ca7a94d0f9bc8
oleksii <alexbuttko@gmail.com>
2022-10-03 15:18:02 +0300

    hw

---
+++ HW_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace HW_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string password = "userlogin";
+            do
+            {
+                Console.WriteLine("Hi, enter your password plese: ");
+                string answer = Console.ReadLine();
+                if(answer == "administrator")
+                {
+                    Console.WriteLine("You are autorization like Administrator");
+                    break;
+                }
+                else if(answer == password)
+                {
+                    Console.WriteLine("You are autorization like User");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("password is incorrect");
+                }
+            }while(true);
+        }
+    }
+}
---
+++ HW_5_2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace HW_5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] radiusMass = new int[10] { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+
+            do
+            {
+                bool isWin = false;
+                var x = random.Next(-50, 50);
+                var y = random.Next(-50, 50);
+                var a = Math.Sqrt(x * x + y * y);
+
+                for (int i = 1; i < radiusMass.Length; i += 2)
+                {
+                    if (a <= radiusMass[i] && a > radiusMass[i - 1])
+                    {
+                        Console.WriteLine($"You win {10 - (i - 1)}");
+                        isWin = true;
+                    }
+                }
+
+                if (!isWin)
+                {
+                    Console.WriteLine("You lose");
+                }
+                Console.WriteLine("Exit? /n(Yes / No)");
+            } while (Console.ReadLine().ToLower().Contains("n"));
+        }
+
+    }
+}
